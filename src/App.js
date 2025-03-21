75d2401376eb1333f28265c3e8af82cbea356824
Maan Almazroei <AlmazroeiMaan@gmail.com>
2025-03-21 16:16:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import SignUp from "./components/SignUp/SignUp";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import Tickets from "./components/Tickets/Tickets";
+import TicketForm from "./components/TicketForm/TicketForm";
+import Appointments from "./components/Appointments/Appointments";
+import AppointmentForm from "./components/AppointmentForm/AppointmentForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/tickets" element={<Tickets />} />
+        <Route path="/tickets/new" element={<TicketForm />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/appointments/new" element={<AppointmentForm />} />
+      </Routes>
+    </Router>
   );
 }
 
